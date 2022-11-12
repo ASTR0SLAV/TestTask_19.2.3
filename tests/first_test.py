6be79ea03fede3edf02dccb7892a3672b67c8691
ASTR0SLAV <astroslav@icloud.com>
2022-11-12 21:43:46 +0300

    Init

---
+++ tests/first_test.py
@@ -0,0 +1,23 @@
+import pytest
+from app.calculator import Calculator
+
+class TestCalc:
+    def setup(self):
+        self.calc = Calculator
+
+    def test_multiply_culculate_correctly(self):
+        assert self.calc.multiply(self, 2, 2) == 4
+
+    def test_division_culculate_correctly(self):
+        assert self.calc.division(self, 6, 2) == 3
+
+    def test_subtraction_culculate_correctly(self):
+        assert self.calc.subtraction(self, 9, 4) == 5
+
+    def test_adding_culculate_correctly(self):
+        assert self.calc.adding(self, 3, 3) == 6
+
+    def test_multiply_culculate_failed(self):
+        assert self.calc.multiply(self, 2, 2) == 5
+
+
